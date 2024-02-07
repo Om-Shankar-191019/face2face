b8b0c7d88bb0bcc0e2dc96bcb7ea8de8d30cb196
om shankar <om8003golu@gmail.com>
2024-02-07 13:10:34 +0530

    react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+
+const App = () => {
+  const authUser = false;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
