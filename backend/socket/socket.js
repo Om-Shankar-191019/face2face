f687bac454858fc9266d25df41eae0d5e7775bac
om shankar <om8003golu@gmail.com>
2024-02-15 09:35:38 +0530

    deployed url inserted at proxy and socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://vercel.com/om-shankars-projects/face2face"],
     methods: ["GET", "POST"],
   },
 });
