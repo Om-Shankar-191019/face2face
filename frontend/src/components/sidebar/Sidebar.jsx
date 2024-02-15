aa6aa81fb5536fbc27f25b45683525a957f41ca3
om shankar <om8003golu@gmail.com>
2024-02-15 08:58:28 +0530

    widhth adjusted

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import Logout from "../options/Logout";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-full sm:w-80 border-r border-slate-500 px-1 flex flex-col overflow-auto">
+    <div className="h-screen w-full sm:w-64 md:w-80 lg:w-96 border-r border-slate-500 px-1 flex flex-col overflow-auto">
       <div className="flex justify-between items-center bg-themeColor sm:bg-white text-white sm:text-black">
         <h2 className=" py-2 px-2 font-semibold text-lg ">Chats</h2>
         <Logout />
