2573f2eeb56deeb402d927a15bc1ce32acd35e60
om shankar <om8003golu@gmail.com>
2024-02-15 07:08:28 +0530

    msg showing on other user fixed

--- frontend/src/components/chatbox/MessageList.jsx
+++ frontend/src/components/chatbox/MessageList.jsx
@@ -3,11 +3,12 @@ import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import useListenMessage from "../../hooks/useListenMessage";
+import { useSelector } from "react-redux";
 
 const MessageList = () => {
   const { loading, messages } = useGetMessages();
-
   useListenMessage();
+  const { selectedConversation } = useSelector((state) => state.chat);
 
   return (
     <div className="bg-gray-200 flex-1 px-2 py-4 overflow-auto">
@@ -18,7 +19,14 @@ const MessageList = () => {
 
       {!loading &&
         messages.length > 0 &&
-        messages.map((item) => <Message key={item._id} messageItem={item} />)}
+        messages.map((item) => {
+          if (
+            item.receiverId === selectedConversation._id ||
+            item.senderId === selectedConversation._id
+          ) {
+            return <Message key={item._id} messageItem={item} />;
+          }
+        })}
 
       {!loading && messages.length === 0 && (
         <p className="text-center text-gray-400">
